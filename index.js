456ca4c77fb96f4cf9ef4d05020c5fea76a45b0c
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 16:09:59 -0700

    Fixed !

--- index.js
+++ index.js
@@ -56,11 +56,11 @@ function processMessage (event) {
     console.log("Received message from senderId: " + senderId);
     console.log("Message is: " + JSON.stringify(message));
 
-    /*if (message.text == "duck me")
+    if (message.text == "duck me")
         sendMessage(senderId, {text: "ducked"});
     else {
         sendMessage(senderId, {text: "duck off"});
-    }*/
+    }
 }
 
 function sendMessage (recipientId, message) {
