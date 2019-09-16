225d1cdccf93b66c4aa384973151baae69ab055f
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 15:59:33 -0700

    Fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 const
     express = require('express'),
-    request = require('request'),
+    //request = require('request'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json());
 
