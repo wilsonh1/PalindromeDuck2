ce1730129c3de25f18d6d08718428098991cabe5
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 16:17:28 -0700

    Update

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 'use strict';
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
-const request = require('request');
+
 const
+    request = require('request'),
     express = require('express'),
     bodyParser = require('body-parser'),
     app = express().use(bodyParser.json());
