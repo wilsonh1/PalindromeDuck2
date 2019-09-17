327facf3ca8db857c52b7e53e790678a41f8c881
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 19:35:57 -0700

    Update

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const
     app = express().use(bodyParser.json());
 
 const mongoose = require('mongoose');
-var db = mongoose.connect(process.env.MONGODB_URI);
-var Leaderboard = require("./models/leaderboard");
-var Palindrome = require("./models/palindrome");
+var db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+var Leaderboard = require('./models/leaderboard');
+var Palindrome = require('./models/palindrome');
 
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
