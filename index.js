49e36bc4b052e7d8067b466fe559b5b9fbeb6d18
Wilson Ho <wilson.ho2718@gmail.com>
2019-09-16 23:31:47 -0700

    Re

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+/*'use strict';
 
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
@@ -72,18 +72,23 @@ function processMessage (event) {
             date.setSeconds(0, 0);
             date = new Date(date.toLocaleString("en-US", {timeZone: "America/Los_Angeles"}));
 
-            //if (checkPalindrome(date)) {
-        //        sendMessage(senderId, {text: "duck"});
-                /*Palindrome.create({timestamp: date}, function (err, docs) {
+
+            if (checkPalindrome(date)) {
+                sendMessage(senderId, {text: "duck"});
+                Palindrome.create({timestamp: date}, function (err, docs) {
                     if (err)
                         sendMessage(senderId, {text: "palindrome already claimed"});
                     else {
                         Leaderboard.create({})
                     }
-                })*/
-            //}
-        //    else
-            //    sendMessage(senderId, {text: "not a palindrome"});
+                })
+            }
+            else
+                sendMessage(senderId, {text: "not a palindrome"});
+
+
+
+
         }
         else {
             var rand = Math.floor(Math.random() * 2);
@@ -117,7 +122,7 @@ function getName (senderId) {
     };
 }
 
-/*function checkPalindrome (cur) {
+function checkPalindrome (cur) {
     var hour = cur.getHours();
 	if (hour > 12)
 		hour -= 12;
@@ -137,7 +142,7 @@ function getName (senderId) {
 			return false;
 	}
 	return true;
-}*/
+}
 
 function sendMessage (recipientId, message) {
     request({
@@ -151,5 +156,98 @@ function sendMessage (recipientId, message) {
     }, function (err, response, body) {
         if (err)
             console.log("Error sending messages: " + err);
+    });
+}*/
+
+'use strict';
+
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
+
+const
+    request = require('request'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express().use(bodyParser.json());
+
+const mongoose = require('mongoose');
+var db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true});
+var Leaderboard = require('./models/leaderboard');
+var Palindrome = require('./models/palindrome');
+
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
+
+app.post('/webhook', (req, res) => {
+    let body = req.body;
+
+    if (body.object === 'page') {
+        body.entry.forEach(function(entry) {
+            entry.messaging.forEach(function(event) {
+                if (event.message)
+                    processMessage(event);
+            });
+        });
+        res.status(200).send('EVENT_RECEIVED');
+    }
+    else {
+        res.sendStatus(404);
+    }
+});
+
+app.get("/", function (req, res) {
+  res.send("Deployed!");
+});
+
+app.get('/webhook', (req, res) => {
+    const VERIFY_TOKEN = process.env.VERIFICATION_TOKEN;
+
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    if (mode && token) {
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+        }
+        else {
+            res.sendStatus(403);
+        }
+    }
+});
+
+function processMessage (event) {
+    if (!event.message.is_echo) {
+        var senderId = event.sender.id;
+        var message = event.message;
+        var sent = event.timestamp;
+
+        console.log("Received message from senderId: " + senderId);
+        console.log("Message is: " + JSON.stringify(message));
+        console.log("Message sent: " + sent);
+
+        if (message.text == "duck me")
+            sendMessage(senderId, {text: "ducked"});
+        else {
+            var rand = Math.floor(Math.random() * 2);
+            if (rand == 0)
+                sendMessage(senderId, {text: "duck off"});
+            else
+                sendMessage(senderId, {text: "go duck yourself"});
+        }
+    }
+}
+
+function sendMessage (recipientId, message) {
+    request({
+        url: "https://graph.facebook.com/v2.6/me/messages",
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        method: "POST",
+        json: {
+            recipient: {id: recipientId},
+            message: message,
+        }
+    }, function (err, response, body) {
+        if (err)
+            console.log("Error sending messages: " + response.error);
     });
 }
