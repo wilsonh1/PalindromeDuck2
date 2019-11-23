e139b2e5655b0c78ba3e2b3c43929b4dab5adcc7
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2019-11-22 19:12:49 -0800

    changing to final schema

--- models/countdown.js
+++ models/countdown.js
@@ -3,9 +3,10 @@ var Schema = mongoose.Schema;
 
 var CountdownSchema = new Schema ({
     count: Number,
-    people: [mongoose.Schema.Types.Mixed],
-    scores: [mongoose.Schema.Types.Mixed], // used to store scores for each individual (maybe consider leaderboard object?)
-    problems: [mongoose.Schema.Types.Mixed] // number of problems can be fixed for now -- maybe 10? 
+    // map of people to scores => Map[String, Number]
+    people: Map,
+    // map of problems to boolean flag (claimed equals true) => Map[Problem, Boolean]
+    problems: Map  
 });
 
 module.exports = mongoose.model("Countodwn", CountdownSchema)
