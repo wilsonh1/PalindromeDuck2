3d89e4d03ef442f6ceecfb5704791722d5a19f51
ConcurrencyPractitioner <yohan.richard.yu@gmail.com>
2019-11-28 20:19:23 -0800

    Finishing up stuff

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const mongoose = require('mongoose');
 var db = mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 var Leaderboard = require('./models/leaderboard');
 var Palindrome = require('./models/palindrome');
+var User = requuire('./models/user');
 
 const ftw = require('./ftw');
 const countdown = require('./countdown');
